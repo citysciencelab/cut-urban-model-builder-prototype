45d4c1de6c8e932fed502fd19ee5098b766e6fab
herzogrh <rico.herzog@outlook.com>
2024-01-29 19:24:41 +0100

    Nuxt setup

---
+++ modellfabrik/app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="app">
+    <NuxtLayout>
+      <v-app>
+        <Navigation />
+        <NuxtPage />
+      </v-app>
+    </NuxtLayout>
+  </div>
+</template>
+
+
---
+++ modellfabrik/components/navigation.vue
@@ -0,0 +1,60 @@
+<template>
+
+        <v-navigation-drawer
+          v-model="drawer"
+          :rail="rail"
+          permanent
+          @click="rail = false"
+          height="100vh"
+        >
+        <v-img
+            src="/img/Logo_Small.png"
+            max-height="100px"
+            contain
+            class="mx-auto"
+            ></v-img>
+
+
+
+          <v-list-item
+            title="Modellfabrik"
+            nav
+          >
+            <template v-slot:append>
+              <v-btn
+                variant="text"
+                icon="mdi-chevron-left"
+                @click.stop="rail = !rail"
+              ></v-btn>
+            </template>
+          </v-list-item>
+  
+          <v-divider></v-divider>
+  
+          <v-list density="compact" nav>
+            <v-list-item prepend-icon="mdi-home" title="Home" value="home"></v-list-item>
+            <v-list-item prepend-icon="mdi-shape" title="Modell" value="account"></v-list-item>
+            <v-list-item prepend-icon="mdi-library" title="Bibliothek" value="account"></v-list-item>
+            <v-list-item prepend-icon="mdi-sun-compass" title="Szenario-Explorer" value="account"></v-list-item>
+            <!--<v-list-item prepend-icon="mdi-toy-brick-marker" title="My Account" value="account"></v-list-item>--->
+            <v-list-item prepend-icon="mdi-account-group-outline" title="Kollaboration" value="users"></v-list-item>
+          </v-list>
+        </v-navigation-drawer>
+
+  </template>
+
+  
+  <script>
+    export default {
+      data () {
+        return {
+          drawer: true,
+          rail: true,
+        };
+      },
+    }
+  </script>
+
+<style scoped>
+
+</style>
