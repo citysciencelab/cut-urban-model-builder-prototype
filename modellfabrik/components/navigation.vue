ab31ecc72bfc716d98ec81b4dbd364cedb6c848a
herzogrh <rico.herzog@outlook.com>
2024-02-02 18:01:45 +0100

    Navigation styled

--- modellfabrik/components/navigation.vue
+++ modellfabrik/components/navigation.vue
@@ -1,58 +1,42 @@
 <template>
-  <v-navigation-drawer
-    height="100vh"
-    border="0"
-    drawer
-
-  >
-    <v-sheet class="pa-4" dark>
-      <v-img src="/img/Logo_Small.png" max-height="100px" contain></v-img>
-      <template v-slot:append>
-        <v-btn
-          variant="text"
-          icon="mdi-chevron-left"
-          @click.stop="rail = !rail"
-        ></v-btn>
-      </template>
-    </v-sheet>
-
-    <v-list nav>
-      <v-list-item
-        v-for="item in navigationItems"
-        :key="item.title"
-        :to="item.to"
-        link
-        :prepend-icon="item.icon"
-        :title="item.title"
-        :value="item.title"
-        class="pa-3"
-      >
-      </v-list-item>
-    </v-list>
+  <v-navigation-drawer border="0" width="120" justify-center>
+    <v-card class="d-flex flex-column justify-top align-center" width="100%" height="100%" color="#ffffff" flat>
+      
+      <v-list lines="two">
+        <v-img src="/img/Logo_Small.png" width="60"  class="mt-5 mb-10"></v-img>
+        <v-list-item v-for="item in navigationItems" :key="item.title" :to="item.to" :prepend-icon="item.icon"
+          :value="item.title" :color="item.color" class="mb-2" width="60px" slim link>
+        </v-list-item>
+      </v-list>
+    </v-card>
   </v-navigation-drawer>
 </template>
 
     
-    <script>
+<script>
 export default {
   data() {
     return {
-        rail: true,
       navigationItems: [
-        { title: "Home", icon: "mdi-home", to: "/", color:"#a6c1e6" },
-        { title: "Modell", icon: "mdi-shape", to: "/model", color:"#f8aa36"},
-        { title: "Bibliothek", icon: "mdi-library", to: "/library", color:"#7abf85"},
+        { title: "Home", icon: "mdi-home", to: "/", color: "#a6c1e6" },
+        { title: "Modell", icon: "mdi-shape", to: "/model", color: "#f8aa36" },
+        {
+          title: "Bibliothek",
+          icon: "mdi-library",
+          to: "/library",
+          color: "#7abf85",
+        },
         {
           title: "Szenario-Explorer",
           icon: "mdi-sun-compass",
           to: "/scenario",
-            color:"#475166"
+          color: "#475166",
         },
         {
           title: "Kollaboration",
           icon: "mdi-account-group-outline",
           to: "/collaboration",
-            color:"#c3a9cf"
+          color: "#c3a9cf",
         },
       ],
     };
@@ -60,8 +44,4 @@ export default {
 };
 </script>
 
-<style scoped>
-.v-list-item {
-  margin: 15px;
-}
-</style>
+<style scoped></style>
